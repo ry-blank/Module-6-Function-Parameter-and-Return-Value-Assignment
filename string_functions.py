0bd600f8c21d3b5abacc8bd4c41cfae3bb14c93c
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-09-29 15:23:08 -0500

    Function Parameter and Return Value Assignment
    Final commit

--- string_functions.py
+++ string_functions.py
@@ -1,5 +1,11 @@
 def multiply_string(message, n):
-    pass
+    """
+    Multiply the parameters and return result
+    :param message: message to be multiplied by n
+    :param n: number of times to multiply message by
+    :return: result of message multiplied by n
+    """
+    return message * n
 
 
 if __name__ == '__main__':
